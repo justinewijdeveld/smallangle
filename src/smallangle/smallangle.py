3352ad4a453cb142e74303c4e849df5d453a78fd
justinewijdeveld <justinewijdeveld@hotmail.com>
2024-11-14 12:02:32 +0100

    create smallangle
    smallangle lets user choose between sin and tan and how many steps. i lost all my commits, sorry. the code does not work yet

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,13 +3,31 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def smallangle():
+    pass
 
+@smallangle.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help='Number of steps between 0 and 2π',
+    show_default=True,
+    type=int)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@smallangle.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help='Number of steps between 0 and 2π',
+    show_default=True,
+    type=int)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
